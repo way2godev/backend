539d8e017bb4974cd2c19d7668a5883ce2962192
Albert Bru Corts <albertbru04@gmail.com>
2024-05-02 15:19:27 +0200

    Added GTFS_STOP_TIMES_FILE constant and removed GTFS PROCESSING CHUNK SIZE

--- constants/constants.go
+++ constants/constants.go
@@ -8,6 +8,6 @@ const GTFS_AGENCIES_FILE = "agency.txt"
 const GTFS_ROUTES_FILE = "routes.txt"
 const GTFS_TRIPS_FILE = "trips.txt"
 const GTFS_STOPS_FILE = "stops.txt"
+const GTFS_STOP_TIMES_FILE = "stop_times.txt"
 
-const GTFS_PROCESSING_CHUNK_SIZE = 300
 const GTFS_PROCESSING_CHUNKS = 100
